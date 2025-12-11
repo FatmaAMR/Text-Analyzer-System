9756d19322d8400fbc91bfc2249bef18a526771d
MalakAlaa2004 <129285060+MalakAlaa2004@users.noreply.github.com>
2025-12-11 19:30:43 +0200

    Add files via upload

---
+++ text-analyzer-db/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open System.Windows.Forms
+open TextAnalyzerSystem.UI
+
+[<STAThread>]
+[<EntryPoint>]
+let main argv =
+    try
+        Database.initializeDatabase()
+        Application.EnableVisualStyles()
+        Application.Run(new MainForm())
+        0
+    with
+    | ex ->
+        printfn "CRITICAL ERROR: %O" ex
+        MessageBox.Show($"Application crashed: {ex.ToString()}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error) |> ignore
+        1
